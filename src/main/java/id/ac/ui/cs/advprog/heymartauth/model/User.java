807b33b4d7cb9747c75a11c833f20bdd234f9bd3
ArvinCS <arvinciu86@yahoo.com>
2024-05-01 20:09:48 +0700

    [REFACTOR] getUsername return String userId

--- src/main/java/id/ac/ui/cs/advprog/heymartauth/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartauth/model/User.java
@@ -41,7 +41,7 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return this.email;
+        return String.valueOf(this.id);
     }
 
     @Override
