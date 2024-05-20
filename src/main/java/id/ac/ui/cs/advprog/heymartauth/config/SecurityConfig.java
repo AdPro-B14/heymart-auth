87eab81cbbd346f28912365f9f38ff60685b8cc8
ArvinCS <arvinciu86@yahoo.com>
2024-05-20 16:13:24 +0700

    [REFACTOR] Profile endpoint

--- src/main/java/id/ac/ui/cs/advprog/heymartauth/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartauth/config/SecurityConfig.java
@@ -25,7 +25,9 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
             }
         };
     }
